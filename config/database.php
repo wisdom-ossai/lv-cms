716d84a6655fa838523044ef8d64faa15cba218a
unknown <chukwudio@xceed365.com>
2020-08-01 11:14:28 +0100

    change database url

--- config/database.php
+++ config/database.php
@@ -51,9 +51,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => $url,
             'host' => $host,
-            'port' => $url,
+            'port' => env('DB_PORT', '3306'),
             'database' => $database,
             'username' => $username,
             'password' => $password,
