8794bfa5c9d03003b3ba5686ed1dcaa96bd69538
unknown <chukwudio@xceed365.com>
2020-08-11 16:42:24 +0100

    Remove Post Seeder call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(PostSeeder::class);
+        // $this->call(PostSeeder::class);
     }
 }
